ac9e45e9f0d6fe782efffc0806a76976c1c47647
Perp-pexel <perpaman44@gmail.com>
2024-11-12 13:57:20 +0000

    rbac

--- utils/rbac.js
+++ utils/rbac.js
@@ -11,27 +11,5 @@ export const permissions = [
             'delete_review',
             
         ]
-    },
-    {
-        role: 'staff',
-        actions: [
-            'get_profile',
-            'update_profile',
-            'post_staff',
-            'get_staff',
-            'get_staff',
-            'delete_staff',
-            'update_staff',
-        ] 
-    },
-    {
-        role: 'admin',
-        actions: [
-            'get_profile',
-            'update_profile',
-            'get_staff',
-            'get_staff',
-            'get_review',
-        ] 
     }
 ]
