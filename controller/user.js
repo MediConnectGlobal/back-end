6a89a433334e395b8e94664d3e25019efef5a872
Perp-pexel <perpaman44@gmail.com>
2024-11-21 23:49:40 +0000

    update

--- controller/user.js
+++ controller/user.js
@@ -118,8 +118,9 @@ export const logOutUser= (req, res, next) => {
     res.status(200).json('User checked-out')
 }
 
-export const updateProfile= (req, res, next) => {
+export const updateProfile= async (req, res, next) => {
     try {
+        const user = await UserModel.findById(req.params.id);
         // Validate user input
         const {error, value} = updateUserValidator.validate(req.body);
         res.status(200).json({message:'User profile updated', user})
